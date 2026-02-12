c409fd4bf7ea50a4611edb9b283e26c68e994d16
Mayaaalborg <maaa0002@stud.kea.dk>
2026-02-12 14:27:21 +0100

    find5fejl
    bipbop

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -1,24 +1,24 @@
 
 
-public class Main {
+public class Main
 
     public static void main(String[] args) {
 
         String name = "Maya";
-        int age = 24;
+        int age = "24";
 
         System.out.println("Hello, my name is " + name);
         System.out.println("I am " + age + " years old.");
 
         System.out.println("Counting to 5:");
-        for (int i = 1; i <= 5; i++) {
+        for (int i = 1; i <= 5; i--) {
             System.out.println(i);
         }
 
-        greet("Welcome to the project!");
+        greet(Welcome to the project!);
     }
 
-    public static void greet(String message) {
+    public static void greet(String message)
         System.out.println(message);
     }
 }
